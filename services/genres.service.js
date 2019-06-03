d6c9ed6ce36570fa0eac2c97a96ae17cc1aca9b7
Matheus Jacob Paulin <matheus_paulin11@hotmail.com>
2019-06-03 14:37:02 +0700

    Genres service + db.mixin

---
+++ services/genres.service.js
@@ -0,0 +1,75 @@
+"use strict";
+
+const DbService = require("../mixins/db.mixin.js");
+
+
+module.exports = {
+	name: "genres",
+	mixins: [
+		DbService("genres"),
+	],
+	/**
+	 * Service settings
+	 */
+	settings: {
+		fields: ["_id", "title", "description"],
+		entityValidator: {
+			title: "string",
+			description: "string",
+		}
+	},
+
+	/**
+	 * Service metadata
+	 */
+	metadata: {
+
+	},
+
+	/**
+	 * Service dependencies
+	 */
+	//dependencies: [],	
+
+	/**
+	 * Actions
+	 */
+	actions: {
+
+	},
+
+	/**
+	 * Events
+	 */
+	events: {
+
+	},
+
+	/**
+	 * Methods
+	 */
+	methods: {
+
+	},
+
+	/**
+	 * Service created lifecycle event handler
+	 */
+	created() {
+
+	},
+
+	/**
+	 * Service started lifecycle event handler
+	 */
+	started() {
+
+	},
+
+	/**
+	 * Service stopped lifecycle event handler
+	 */
+	stopped() {
+
+	}
+};
