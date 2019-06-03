abe1b4e7b552a498b3a2154d1ed5afa7aa6b5165
Matheus Jacob Paulin <matheus_paulin11@hotmail.com>
2019-06-04 00:24:05 +0700

    Create + get tests

--- test/unit/genres.spec.js
+++ test/unit/genres.spec.js
@@ -5,37 +5,155 @@ const { ValidationError } = require("moleculer").Errors;
 const TestService = require("../../services/genres.service");
 const rimraf = require("rimraf");
 
-describe("Test 'genres' service", () => {
+function seedDB(broker){
+	const genresToCreate = [
+		{title : "Romantic", description : "Romantic description"},
+		{title : "Drama", description : "Drama description"},
+		{title : "Action", description : "Action description"},
+		{title : "Thriller", description : "Thriller description"}
+	];
+
+	const genresPromises = [];
+	genresToCreate.forEach(function(e){
+		genresPromises.push(broker.call("genres.create",e));
+	});
+
+	return Promise.all(genresPromises);
+}
+
+let broker = {};
+
+beforeAll(() => {
 	delete process.env.MONGO_URI;
-	rimraf.sync("../../data/");
 
-	let broker = new ServiceBroker();
-	const genresService = broker.createService(TestService);
-	
-	beforeAll(() => {
-		broker.start();
-	});
-	afterAll(() => broker.stop());
+	rimraf("data/",function(err){
+		if(err)
+			console.log("ERROR : " , err);
+	});	
 
-	describe("Test 'greeter.hello' action", () => {
-
-		it("should return with 'Hello Moleculer'", () => {
-			expect(broker.call("greeter.hello")).resolves.toBe("Hello Moleculer");
-		});
-
-	});
-
-	describe("Test 'greeter.welcome' action", () => {
-
-		it("should return with 'Welcome'", () => {
-			expect(broker.call("greeter.welcome", { name: "Adam" })).resolves.toBe("Welcome, Adam");
-		});
-
-		it("should reject an ValidationError", () => {
-			expect(broker.call("greeter.welcome")).rejects.toBeInstanceOf(ValidationError);
-		});
-
-	});
-
+	broker = new ServiceBroker();
+	broker.createService(TestService);
+	return broker.start();
 });
+
+afterAll(() => broker.stop());
+
+describe("Test 'genres' service", () => {
+
+	describe("Test 'Genres.insert' action", () => {
+
+		it("should return validation error, if sent without parameters", () => {
+			return broker.call("genres.create",{})
+				.catch((e) => {
+					expect(e).toBeInstanceOf(ValidationError);
+					expect(e).toMatchObject({code : 422});
+				});
+			
+		});
+
+		it("should return validation error, if sent without mandatory fields", () => {
+			return broker.call("genres.create",{ description : "Genre Description"})
+				.catch((e) => {
+					expect(e).toBeInstanceOf(ValidationError);
+					expect(e).toMatchObject({code : 422});
+				});
+		});
+
+
+		it("should return validation error, if sent with wrong type in description", () => {
+			return broker.call("genres.create",{ title: "Genre Title" , description : 2})
+			.catch((e) => {
+				expect(e).toBeInstanceOf(ValidationError);
+				expect(e).toMatchObject({code : 422});
+				expect(e.data).toEqual(expect.arrayContaining([expect.objectContaining({type : "string"})]));
+			})
+		});
+
+		it("should return validation error, if sent with wrong type in title", () => {
+			return broker.call("genres.create",{ title: 2})
+			.catch((e) => {
+				expect(e).toBeInstanceOf(ValidationError);
+				expect(e).toMatchObject({code : 422});
+				expect(e.data).toEqual(expect.arrayContaining([expect.objectContaining({type : "string"})]));
+			})
+		});
+
+		it("should succeed", () => {
+			return broker.call("genres.create",{ title: "Genre TItle" , description : "Genre description"})
+			.then((data) => {
+				expect(data).toHaveProperty("_id");
+				expect(data).toHaveProperty("title");
+				expect(data).toHaveProperty("description");
+			})
+		});
+	});
+
+	describe("Test 'Genres.get' action", () => {
+
+		beforeAll(() => {
+			rimraf("data/",function(err){
+				if(err)
+					console.log("ERROR : " , err);
+			});	
+		
+			return broker.start()
+			.then(()=>{ 
+				return seedDB(broker);
+			});
+		});
+
+		it("should return validation error, if sent without parameters", () => {
+			return broker.call("genres.get",{})
+			.catch((e) => {
+				expect(e).toBeInstanceOf(ValidationError);
+				expect(e).toMatchObject({code : 422});
+				expect(e.data).toEqual(expect.arrayContaining([expect.objectContaining({type : "required",field: "id"})]));
+			})
+		});
+		
+		it("should get a specific existent genre", () => {
+
+			let genreToCompare = {};
+			return broker.call("genres.list",{ populate : [], fields: ["_id","title","description"], searchFields : []})
+			.then((data) => {
+				expect(data).toHaveProperty("total");
+				expect(data.total).toEqual(4);
+				genreToCompare = data.rows[0];
+				return broker.call("genres.get",{ populate : [], fields: ["_id","title","description"], id: data.rows[0]._id});
+			})
+			.then((data)=>{
+				expect(data).toHaveProperty("_id");
+				expect(data).toHaveProperty("title");
+				expect(data).toHaveProperty("description");
+				expect(data).toEqual(genreToCompare);
+			});
+		});
+
+		it("should get a specific existent genre with only ID field", () => {
+
+			return broker.call("genres.list",{ populate : [], fields: ["_id"], searchFields : []})
+			.then((data) => {
+				expect(data).toHaveProperty("total");
+				expect(data.total).toEqual(4);
+				return broker.call("genres.get",{ populate : [], fields: ["_id"], id: data.rows[0]._id});
+			})
+			.then((data)=>{
+				expect(data).toHaveProperty("_id");
+				expect(data).not.toHaveProperty("title");
+				expect(data).not.toHaveProperty("description");
+			});
+		});
+
+		it("should return EntityNotFoundError for entity not found ", () => {
+
+			return broker.call("genres.get",{ populate : [], fields: ["_id","title","description"], id: 1})
+			.catch((e)=>{
+				console.log(e);
+				expect(e).toMatchObject({code : 404});
+			});
+		});
+	});
+});
+
+
 
